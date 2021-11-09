145876a88a6ecc3f1ea22ebe1d54629619fb1bbc
Pavel Khnykin <mazenrab@rambler.ru>
2021-11-09 18:26:46 +0300

    Kills processes of a specific user.

---
+++ killprocesslist.ps1
@@ -0,0 +1,17 @@
+﻿$username = $args[0]
+$processes = $args[1]
+
+foreach ($process in $processes.Split(','))
+{
+    $p = $process.Trim();
+
+    try
+    {
+    Get-Process $p -IncludeUserName | Where UserName -Match $username | ForEach-Object {Stop-Process -InputObject $_ -Force}
+    }
+    catch
+    {
+    $ErrorMessage = $_.Exception.Message
+    Write-Output $ErrorMessage
+    }    
+}
